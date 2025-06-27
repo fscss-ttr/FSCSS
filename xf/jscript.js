0bce0546a03b3fafbf1c4bc8c5c33b6489879441
Figsh <safetyunion5550@gmail.com>
2025-06-27 13:07:42 +0100

    Update jscript.js

--- xf/jscript.js
+++ xf/jscript.js
@@ -1,2 +1,5 @@
-import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.0/xfscss.min.js";
-new exec("text"," ");
+import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.6/e/xfscss.min.js";
+new exec({
+  type:"URL"
+    content: "styles/style.css";
+    );
