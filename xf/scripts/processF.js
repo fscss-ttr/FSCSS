0e38163338e3aa752be6ef7b062d4567ae75b32b
Figsh <safetyunion5550@gmail.com>
2026-02-26 13:13:46 +0100

    Update processF.js

--- xf/scripts/processF.js
+++ xf/scripts/processF.js
@@ -1,4 +1,4 @@
-import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.6/e/xfscss.min.js";
+import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.13/e/xfscss.min.js";
 new exec({
   type:"URL", 
     content: "https://cdn.jsdelivr.net/gh/fscss-ttr/FSCSS@main/xf/styles/style.css"
