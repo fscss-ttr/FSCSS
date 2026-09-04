fea78cdffc9df95df4eca9ae4545bddac26fd4e1
Figsh <safetyunion5550@gmail.com>
2026-09-04 18:50:06 +0100

    Delete xf/scripts/processF.js

--- xf/scripts/processF.js
+++
@@ -1,5 +0,0 @@
-import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.24/e/xfscss.min.js";
-new exec({
-  type:"URL", 
-    content: "https://cdn.jsdelivr.net/gh/fscss-ttr/FSCSS@main/xf/styles/style.css"
-   });
